edb9a62057b79757e48c5970979aea7ff7512a61
shoalwave <shoalwave.dev@gmail.com>
2022-10-12 12:47:07 +0900

    fix

--- includes/library/AppError.php
+++ includes/library/AppError.php
@@ -2,12 +2,12 @@
 
 class AppError {
     private static $message = [
-        1 =>    'You have no referer.',
-        2 =>    'The referer is invalid.',
-        3 =>    'The token is incorrect.',
-        4 =>    'There are function references from invalid locations.',
-        5 =>    'Failed to create directory for log file output. Please create the directory manually.',
-        6 =>    'Failed to write log csv.',
+        1 => 'You have no referer.',
+        2 => 'The referer is invalid.',
+        3 => 'The token is incorrect.',
+        4 => 'There are function references from invalid locations.',
+        5 => 'Failed to create directory for log file output. Please create the directory manually.',
+        6 => 'Failed to write log csv.',
     ];
 
     private function __construct() {}
--- view/template-parts/header.php
+++ view/template-parts/header.php
@@ -8,14 +8,17 @@
     <meta name="viewport"
         content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
     <meta name="format-detection" content="telephone=no">
+    <!-- page title -->
     <?php if( ! empty( $favicon ) ): ?>
     <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="<?php echo $favicon; ?>">
     <?php endif; ?>
     <title><?php echo $page_title; ?></title>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&family=Noto+Serif+JP:wght@200;300;400;500;600;700;900&display=swap"
-        rel="stylesheet">
-    <link href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" rel="stylesheet">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&family=Noto+Serif+JP:wght@200;300;400;500;600;700;900&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.0.0/css/all.css">
     <!-- user`s stylesheets -->
     <?php foreach( $css_links as $link ) : ?>
     <link href="<?php echo $link; ?>" rel="stylesheet" type="text/css">
