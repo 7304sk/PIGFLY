cf1ec56df737b8523e601e34fa5660723e3000d6
shoalwave <shoalwave.dev@gmail.com>
2022-12-23 16:17:19 +0900

    スクリプト読み込みの整理（loader.phpへの一元化）

--- includes/loader.php
+++ includes/loader.php
@@ -1,26 +1,20 @@
 <?php
-/** クラスローダー */
-class ClassLoader {
-    protected $dirs;
+/** config.php の読み込み */
+require_once APP_PATH . 'config.php';
 
-    public function set( $dir ) {
-        $this->dirs[] = $dir;
-    }
+/** 設定によるその他設定の定義・上書き */
+define( 'APP_URL', ( ( ( ! empty( $_SERVER[ 'HTTPS' ] ) && $_SERVER[ 'HTTPS' ] !== 'off' ) ) ? 'https://' : 'http://' ) . $_SERVER[ 'HTTP_HOST' ] . $_SERVER[ 'REQUEST_URI' ] );
+array_push( $mandatory, $user_email );
+if( $mode_email_retype && ! empty( $email_retype ) ) array_push( $mandatory, $email_retype );
+if( $mode_test ) ini_set('display_errors', 1);
+if( empty( $mail_sender ) ) $mail_sender = 'no-reply@' . $_SERVER[ 'HTTP_HOST' ];
+if( empty( $page_thanks ) ) $page_thanks = ( ( ( ! empty( $_SERVER[ 'HTTPS' ] ) && $_SERVER[ 'HTTPS' ] !== 'off' ) ) ? 'https://' : 'http://' ) . $_SERVER[ 'HTTP_HOST' ];
 
-    public function start() {
-        spl_autoload_register( array( $this, 'loadClass' ) );
-    }
-
-    public function loadClass( $class ) {
-        foreach( $this->dirs as $dir ) {
-            $file = $dir. '/' . $class . '.php';
-            if( is_readable( $file ) ) {
-                require $file;
-                return;
-            }
-        }
-    }
-}
+/** クラスローダーの実行 */
+require APP_PATH . 'includes/library/ClassLoader.php';
+$loader = new ClassLoader();
+$loader->set( APP_PATH . 'includes/library' );
+$loader->start();
 
 /** その他ファイルの読み込み */
 $replace_map = require_once APP_PATH . 'includes/replace.php';
