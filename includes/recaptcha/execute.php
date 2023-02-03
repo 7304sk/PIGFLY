6cbebed6da23dc109213ae97987211a38425b995
shoalwave <shoalwave.dev@gmail.com>
2023-02-04 00:50:00 +0900

    reCAPTCHA 実行機能の微修正（タイムアウトにならないように）

--- includes/recaptcha/execute.php
+++ includes/recaptcha/execute.php
@@ -3,11 +3,17 @@ ini_set('display_errors', 1);
 require_once __DIR__ . '/../../config.php';
 
 $script = <<< EOT
-grecaptcha.ready(function () {
-    grecaptcha.execute('${recaptcha_site_key}', {action: 'submit'}).then(function(token) {
-        const recaptchaResponse = document.getElementById('recaptchaResponse');
-        recaptchaResponse.value = token;
-    });
+const recaptchaResponse = document.getElementById('recaptchaResponse');
+const recaptchaForm = recaptchaResponse.closest('form');
+
+recaptchaForm.addEventListener('submit', e => {
+    e.preventDefault();
+        grecaptcha.ready(function () {
+            grecaptcha.execute('${recaptcha_site_key}', {action: 'submit'}).then(function(token) {
+                recaptchaResponse.value = token;
+                recaptchaForm.submit();
+            });
+        });
 });
 EOT;
 
